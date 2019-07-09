8b15e3aba5b984ebe38dd26c040f99d968908436
Rudolf <47896892+nerudxlf@users.noreply.github.com>
2019-07-09 18:59:01 +0600

    Логика калькулятора
    Добавил функционал калькулятору
    Сложение
    Вычитание
    Деление
    Умножение

--- CalculatorOOP/CalculatorOOP/Form1.Designer.cs
+++ CalculatorOOP/CalculatorOOP/Form1.Designer.cs
@@ -44,81 +44,83 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(274, 48);
+            this.button1.Location = new System.Drawing.Point(93, 111);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.Click += new System.EventHandler(this.ButtonPlus);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(274, 92);
+            this.button2.Location = new System.Drawing.Point(93, 140);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.Click += new System.EventHandler(this.ButtonMinus);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(274, 134);
+            this.button3.Location = new System.Drawing.Point(174, 111);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "*";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ButtonMultiply);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(274, 177);
+            this.button4.Location = new System.Drawing.Point(174, 140);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
             this.button4.Text = "/";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            this.button4.Click += new System.EventHandler(this.ButtonDivide);
             // 
             // Value1
             // 
-            this.Value1.Location = new System.Drawing.Point(135, 137);
+            this.Value1.Location = new System.Drawing.Point(93, 76);
             this.Value1.Name = "Value1";
             this.Value1.Size = new System.Drawing.Size(100, 20);
             this.Value1.TabIndex = 4;
+            this.Value1.TextChanged += new System.EventHandler(this.Value1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(590, 136);
+            this.textBox2.Location = new System.Drawing.Point(450, 76);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(101, 20);
             this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Value2
             // 
-            this.Value2.Location = new System.Drawing.Point(399, 136);
+            this.Value2.Location = new System.Drawing.Point(269, 76);
             this.Value2.Name = "Value2";
             this.Value2.Size = new System.Drawing.Size(100, 20);
             this.Value2.TabIndex = 6;
-            this.Value2.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            this.Value2.TextChanged += new System.EventHandler(this.Value2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(535, 131);
+            this.label1.Location = new System.Drawing.Point(413, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(21, 24);
             this.label1.TabIndex = 7;
             this.label1.Text = "=";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(100, 110);
+            this.label2.Location = new System.Drawing.Point(64, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(168, 24);
             this.label2.TabIndex = 8;
@@ -128,7 +130,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(373, 110);
+            this.label3.Location = new System.Drawing.Point(238, 45);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(166, 24);
             this.label3.TabIndex = 9;
@@ -138,18 +140,17 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(588, 109);
+            this.label4.Location = new System.Drawing.Point(470, 49);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(67, 24);
             this.label4.TabIndex = 10;
             this.label4.Text = "Ответ";
-            this.label4.Click += new System.EventHandler(this.Label4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(574, 201);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
--- CalculatorOOP/CalculatorOOP/Form1.cs
+++ CalculatorOOP/CalculatorOOP/Form1.cs
@@ -16,5 +16,53 @@ namespace CalculatorOOP
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Value1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Value2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void ButtonPlus(object sender, EventArgs e)
+        {
+            double numberValueOne = Convert.ToDouble(Value1.Text);
+            double numberValueTwo = Convert.ToDouble(Value2.Text);
+            double numberOtvet = numberValueTwo + numberValueOne;
+            textBox2.Text = numberOtvet.ToString();
+        }
+        private void ButtonMinus(object sender, EventArgs e)
+        {
+            double numberValueOne = Convert.ToDouble(Value1.Text);
+            double numberValueTwo = Convert.ToDouble(Value2.Text);
+            double numberOtvet = numberValueOne - numberValueTwo;
+            textBox2.Text = numberOtvet.ToString();
+        }
+
+        private void ButtonMultiply(object sender, EventArgs e)
+        {
+            double numberValueOne = Convert.ToDouble(Value1.Text);
+            double numberValueTwo = Convert.ToDouble(Value2.Text);
+            double numberOtvet = numberValueTwo * numberValueOne;
+            textBox2.Text = numberOtvet.ToString();
+        }
+        private void ButtonDivide(object sender, EventArgs e)
+        {
+            double numberValueOne = Convert.ToDouble(Value1.Text);
+            double numberValueTwo = Convert.ToDouble(Value2.Text);
+            double numberOtvet = numberValueOne / numberValueTwo;
+            textBox2.Text = numberOtvet.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
